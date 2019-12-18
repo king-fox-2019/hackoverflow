65ab6cbc2c10b7a16c9ee9d7a2563e6f30a0b3d7
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-12-18 15:13:10 +0700

    Add Navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="accent" fixed="top">
+    <b-navbar-brand href="#">Stun overflow</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto" v-if="$store.state.onSession">
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto" v-else>
+        <b-nav-item-dropdown :text="$store.state.email" right>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  computed: {}
+}
+</script>
+
+<style></style>
