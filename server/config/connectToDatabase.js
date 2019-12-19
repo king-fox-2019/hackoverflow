5e0ed99cf16512b6ca70878a3214142cd74f76f6
SYX <Syirilrakhulhaqim@gmail.com>
2019-12-19 18:06:26 +0700

    change database to atlas

--- server/config/connectToDatabase.js
+++ server/config/connectToDatabase.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose.connect(
-    "mongodb://localhost:27017/hacktivOverflow",
+    // "mongodb://localhost:27017/hacktivOverflow",
+    "mongodb+srv://foobar:foobar@cluster0-2ytp0.mongodb.net/test?retryWrites=true&w=majority",
     {
       useFindAndModify: false,
       useNewUrlParser: true,
