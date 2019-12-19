c0c22f3f34b585a5d9696a01807dbd78e440af28
risanto <potallegta@gmail.com>
2019-12-19 18:27:29 +0700

    Removed slash in client/config/axios

--- client/configs/axios.js
+++ client/configs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'http://ec2-3-0-183-33.ap-southeast-1.compute.amazonaws.com/',
+  baseURL: 'http://ec2-3-0-183-33.ap-southeast-1.compute.amazonaws.com',
   headers: {
     access_token: localStorage.getItem('access_token')
   }
