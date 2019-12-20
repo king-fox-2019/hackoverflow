e6acb5a7a9bccb4dff8b33ea0ee92f29d9fd5a60
SYX <Syirilrakhulhaqim@gmail.com>
2019-12-20 10:46:22 +0700

    change address baseurl

--- server/config/connectToDatabase.js
+++ server/config/connectToDatabase.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose.connect(
-    "mongodb://localhost:27017/hacktivOverflow",
-    // process.env.MONGODB_ATLAS,
+    // "mongodb://localhost:27017/hacktivOverflow",
+    process.env.MONGODB_ATLAS,
     {
       useFindAndModify: false,
       useNewUrlParser: true,
