12da6f8507636725ed9c613d0e03027f36760830
anggabanny <anggabanny@gmail.com>
2020-02-05 09:28:23 +0700

    change gcs to unggah

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,23 +1,23 @@
 const routes = require('express').Router()
 const userController = require('../controllers/user')
-const gcsUpload = require('gcs-upload')
 const { authentication } = require('../middlewares/auth')
-
-const upload = gcsUpload({
-    limits: {
-        fileSize: 1e6 // in bytes
+const unggah = require('unggah')
+const storageConfig = unggah.gcs({
+    keyFilename: process.env.GOOGLE_CLOUD_KEYFILE, // this can also be set using GOOGLE_APPLICATION_CREDENTIALS environment variable
+    bucketName: process.env.CLOUD_BUCKET,
+    rename: (req, file) => {
+        return `${Date.now()}-${file.originalname}`;
     },
-    gcsConfig: {
-        keyFilename: "keyfile.json",
-        bucketName: "imagebucket-stunoverflow"
-    }
-})
+});
+
+const upload = unggah({
+    storage: storageConfig
+});
 
 routes.post('/register', upload.array('image') ,userController.register)
 routes.post('/login', userController.login)
 routes.use(authentication)
 routes.get('/myaccount', userController.findOne)
 routes.patch('/addWatchedTags', userController.updateWatchedTags)
-
 
 module.exports = routes
